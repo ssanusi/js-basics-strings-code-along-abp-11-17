f87349bdb2f1eb38765dd1bb44a918ea5eb9e3a5
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-07 10:17:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'sanusi';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `${currentUser.toUpperCase()} WELCOME TO FLATBOOK, !`
+let shortGreeting = `Welcome, ${currentUser[0]}!`
